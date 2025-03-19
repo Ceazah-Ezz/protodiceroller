7a3ea8df37e59e2c2436f61ea20d17b5290b422e
Ceasar/Xaki <151482529+Ceazah-Ezz@users.noreply.github.com>
2025-03-19 12:48:27 +0800

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+import BakunawaRoller from "./DiceImg/BakunawaRoller.png";
+import Dice_1 from "./DiceImg/Dice_1.png";
+import Dice_2 from "./DiceImg/Dice_2.png";
+import Dice_3 from "./DiceImg/Dice_3.png";
+import Dice_4 from "./DiceImg/Dice_4.png";
+import Dice_5 from "./DiceImg/Dice_5.png";
+
+
+function App() 
+{
+  var DiceImgs = {
+    Dice_1,
+    Dice_2,
+    Dice_3,
+    Dice_4,
+    Dice_5
+  }
+
+  const [image, setNewImage] = useState(DiceImgs[0])
+
+  const rollDice = () => {
+    //Randomly select dice value
+    var randomNum1 = Math.floor(Math.random() * 5);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <center>
+        <br></br>
+        <h1>This a dice prototype! Test it out!</h1>
+        <h3>Click on the serpent to roll the dice!</h3>
+        <br></br>
+        <br></br>
+        <a href="https://www.youtube.com/watch?v=p8OHccFLeDQ">
+          <img src={BakunawaRoller} />
         </a>
-      </header>
+        <div className=''></div>
+      </center>
     </div>
   );
 }
