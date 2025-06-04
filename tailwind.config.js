f5a8d30158e95ecdc779f58d6edd9706be9d1d75
Minokawa <60136630+Minokawa6@users.noreply.github.com>
2025-06-04 14:26:59 +0800

    Added Tailwind, PostCSS, and Autoprefixer plugins

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.js", "./src/Presentation/Pages/App.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
