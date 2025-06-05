858d2b559e2e156bf6e4eda1767f3110200f37e2
Nathaniel Caballero <nathaniel.caballero@ciit.edu.ph>
2025-06-05 18:17:34 +0800

    small update

--- src/Presentation/Pages/Revamped.js
+++ src/Presentation/Pages/Revamped.js
@@ -25,7 +25,7 @@ function Revamped() {
 
         <div className="flex-row border-2 border-red-500">
           <div className="p-3 bg-gray-500 rounded-[20px] text-white">
-            <h2>HOW TO PLAY</h2>
+            <h2 className="text-white text-4xl font-bold">HOW TO PLAY</h2>
             <hr></hr>
             <li>Click a dice to add in the PlayMat</li>
             <li>Click the “Roll” button to roll</li>
