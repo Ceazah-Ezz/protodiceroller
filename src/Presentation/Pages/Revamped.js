7a72216242dfb61c8cfa708704331bf3fea9a32b
Minokawa <60136630+Minokawa6@users.noreply.github.com>
2025-06-10 13:29:55 +0800

    Fixed Revamp.js

--- src/Presentation/Pages/Revamped.js
+++ src/Presentation/Pages/Revamped.js
@@ -1,8 +1,8 @@
 import "./Revamped.css";
 import DieComponent from "../Components/DieComponent";
-import HowTo from './HowTo';
-import Buttons from './Buttons';
-import History from './History';
+import HowTo from "./HowTo";
+import Buttons from "./Buttons";
+import History from "./History";
 import Playmat from "./Playmat";
 
 function Revamped() {
@@ -18,11 +18,10 @@ function Revamped() {
   // Have yet to get assets - in Gdrive
   return (
     <div className="flex bg-[#011f3f]">
-      
       <div className="hidden md:flex flex-col w-2/5 h-screen border-2 border-blue-500 px-3 py-5">
         <HowTo />
         <Buttons />
-        <History /> 
+        <History />
       </div>
 
       <div className="flex-col w-full h-screen border-2 border-blue-500">
@@ -32,37 +31,20 @@ function Revamped() {
         </div>
 
         {/* Playmat */}
-          <div className="flex flex-row w-full h-auto border-2 border-green-500 md:hidden">
-              <div className="flex-col w-1/2 h-auto border-2 border-green-500 md:hidden">
-                  <p className="text-white"> Title </p>
-              </div>
-              <div className="flex-col w-1/2 h-auto border-2 border-green-500 md:hidden">
-                  <p className="text-white"> Buttons </p>
-              </div>
+        <div className="flex-row w-full h-auto border-2 border-green-500 md:hidden">
+          <div className="flex-col w-1/2 h-auto border-2 border-green-500 md:hidden">
+            <p className="text-white"> Title </p>
+          </div>
+          <div className="flex-col w-1/2 h-auto border-2 border-green-500 md:hidden">
+            <p className="text-white"> Buttons </p>
           </div>
           <div className="flex-row w-full h-auto border-2 border-green-500 md:hidden">
             <p className="text-white"> History </p>
           </div>
-          <Playmat />
-          {/* <D4Component/> */}
-          <DieComponent />
-          {/* Playmat Image */}
-          <img
-            width={1920}
-            height={1080}
-            src={playmat}
-            alt="PlayMat"
-            className="z-0"
-          />
-
-          {/* Roll Button (positioned at bottom-right) */}
-          <button className='absolute bottom-4 right-4 bg-[#05a895] hover:bg-[#048579] text-white font-bold py-2 px-6 rounded-[25px] shadow-lg transition duration-200"'>
-            <img width={100} height={100} src={roll} alt="ROLL!" />
-          </button>
         </div>
+        <Playmat />
       </div>
-      </div>
-    
+    </div>
   );
 }
 
